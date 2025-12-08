f54ebf93ea48fbb3b2a98e21003ac862a348de9f
Fabiano Rosa <fabianorosa1@gmail.com>
2025-12-08 19:07:23 +0100

    Test layout

--- src/main/java/com/example/StructurizrWorkspace.java
+++ src/main/java/com/example/StructurizrWorkspace.java
@@ -1,54 +1,66 @@
 package com.example;
 
 import com.structurizr.Workspace;
+import com.structurizr.annotation.Tag;
+import com.structurizr.autolayout.graphviz.GraphvizAutomaticLayout;
 import com.structurizr.model.*;
 import com.structurizr.util.WorkspaceUtils;
 import com.structurizr.view.*;
 import java.io.File;
 
 public class StructurizrWorkspace {
-  public static void main(String[] args) throws Exception {
-    Workspace workspace = new Workspace("Big Bank plc", "Internet Banking System");
-    Model model = workspace.getModel();
-    ViewSet views = workspace.getViews();
 
-    Styles styles = views.getConfiguration().getStyles();
-    styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
-    styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff").shape(Shape.Person);
+    public static void main(String[] args) throws Exception {
+        Workspace workspace = new Workspace("Big Bank plc", "Internet Banking System");
+        Model model = workspace.getModel();
+        ViewSet views = workspace.getViews();
 
-    Person customer = model.addPerson("Customer", "A customer of the bank.");
-    SoftwareSystem internetBankingSystem = model.addSoftwareSystem("Internet Banking System",
-        "Allows customers to view accounts and make payments.");
-    customer.uses(internetBankingSystem, "Uses");
+        views.getConfiguration().setThemes("default",
+                "https://fabianorosa1.github.io/structurizr-themes/sap-btp-solution-diagrams-2025.12.08/theme.json");
 
-    Container webApp = internetBankingSystem.addContainer("Web Application", "Delivers the web UI.",
-        "Java & Spring MVC");
-    Container database = internetBankingSystem.addContainer("Database", "Stores customer data.", "Oracle");
-    customer.uses(webApp, "Uses");
-    webApp.uses(database, "Reads/Writes");
+        Styles styles = views.getConfiguration().getStyles();
+        styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
+        styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff").shape(Shape.Person);
 
-    DeploymentNode liveWebServer = model.addDeploymentNode("bigbank-web***", "A web server in the farm.",
-        "Ubuntu 16.04 LTS", 8);
-    DeploymentNode tomcat = liveWebServer.addDeploymentNode("Apache Tomcat", "Java web server.", "Tomcat 8.x", 1);
-    tomcat.add(webApp);
+        Person customer = model.addPerson("Customer", "A customer of the bank.");
+        SoftwareSystem internetBankingSystem = model.addSoftwareSystem("Internet Banking System",
+                "Allows customers to view accounts and make payments.");
+        customer.uses(internetBankingSystem, "Uses");
 
-    DeploymentNode primaryDbServer = model.addDeploymentNode("bigbank-db01", "Primary DB server.", "Ubuntu 16.04 LTS",
-        1);
-    DeploymentNode oraclePrimary = primaryDbServer.addDeploymentNode("Oracle - Primary", "Live database.",
-        "Oracle 12c");
-    oraclePrimary.add(database);
+        Container webApp = internetBankingSystem.addContainer("Web Application", "Delivers the web UI.",
+                "Java & Spring MVC");
+        Container database = internetBankingSystem.addContainer("Database", "Stores customer data.", "Oracle");
 
-    DeploymentNode secondaryDbServer = model.addDeploymentNode("bigbank-db02", "Secondary DB server.",
-        "Ubuntu 16.04 LTS", 1);
-    secondaryDbServer.addDeploymentNode("Oracle - Secondary", "Standby database.", "Oracle 12c");
+        customer.uses(webApp, "Uses");
+        webApp.uses(database, "Reads/Writes");
 
-    DeploymentView liveDeployment = views.createDeploymentView(internetBankingSystem, "LiveDeployment",
-        "Live environment deployment");
-    liveDeployment.addAllDeploymentNodes();
-    liveDeployment.enableAutomaticLayout();
+        DeploymentNode liveWebServer = model.addDeploymentNode("bigbank-web***", "A web server in the farm.",
+                "Ubuntu 16.04 LTS", 8);
+        DeploymentNode tomcat = liveWebServer.addDeploymentNode("Apache Tomcat", "Java web server.", "Tomcat 8.x", 1);
+        tomcat.add(webApp);
 
-    WorkspaceUtils.saveWorkspaceToJson(workspace, new File("workspace.json"));
+        DeploymentNode primaryDbServer = model.addDeploymentNode("bigbank-db01", "Primary DB server.",
+                "Ubuntu 16.04 LTS",
+                1);
+        DeploymentNode oraclePrimary = primaryDbServer.addDeploymentNode("Oracle - Primary", "Live database.",
+                "Oracle 12c");
+        oraclePrimary.add(database);
 
-    System.out.println("Workspace JSON generated.");
-  }
+        DeploymentNode secondaryDbServer = model.addDeploymentNode("bigbank-db02", "Secondary DB server.",
+                "Ubuntu 16.04 LTS", 1);
+        secondaryDbServer.addDeploymentNode("Oracle - Secondary", "Standby database.", "Oracle 12c");
+
+        DeploymentView liveDeployment = views.createDeploymentView(internetBankingSystem, "LiveDeployment",
+                "Live environment deployment");
+
+        liveDeployment.enableAutomaticLayout();
+        liveDeployment.addAllDeploymentNodes();
+
+        GraphvizAutomaticLayout graphviz = new GraphvizAutomaticLayout();
+        graphviz.apply(workspace);
+
+        WorkspaceUtils.saveWorkspaceToJson(workspace, new File("workspace.json"));
+
+        System.out.println("Workspace JSON generated.");
+    }
 }

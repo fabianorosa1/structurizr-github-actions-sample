84febc4ad2bbca0953cf28b3cdd18873dd6de232
Fabiano Rosa <fabianorosa1@gmail.com>
2025-12-08 19:57:41 +0100

    Test

---
+++ src/main/java/com/example/WebshopWorkspace.java
@@ -0,0 +1,123 @@
+package com.example;
+
+import com.structurizr.Workspace;
+import com.structurizr.model.*;
+import com.structurizr.view.*;
+import java.io.File;
+
+public class WebshopWorkspace {
+
+    public static void main(String[] args) throws Exception {
+
+        Workspace workspace = new Workspace("My Webshop Workspace", "Generated from DSL");
+        Model model = workspace.getModel();
+        ViewSet views = workspace.getViews();
+
+        // ---------------------------------------------------------
+        // MODEL
+        // ---------------------------------------------------------
+
+        // People
+        Person customer = model.addPerson("Customer", "The customer of our webshop");
+        Person administrator = model.addPerson("Administrator", "The administrator of the webshop");
+
+        // External system
+        SoftwareSystem globalPayment = model.addSoftwareSystem(
+                "Global Payment",
+                "Used for all banking transactions"
+        );
+
+        // Software System: My Webshop
+        SoftwareSystem myWebshop = model.addSoftwareSystem(
+                "My Webshop",
+                "Our beautiful webshop"
+        );
+        myWebshop.addTags("SAP L0");
+
+        // Containers
+        Container customerFrontend = myWebshop.addContainer(
+                "customerFrontend",
+                "The frontend for the customer",
+                ""
+        );
+
+        Container administratorFrontend = myWebshop.addContainer(
+                "administratorFrontend",
+                "The frontend for the administrator",
+                ""
+        );
+
+        Container webshopBackend = myWebshop.addContainer(
+                "webshopBackend",
+                "The webshop backend",
+                ""
+        );
+
+        Container webshopDatabase = myWebshop.addContainer(
+                "webshopDatabase",
+                "The webshop database",
+                ""
+        );
+        webshopDatabase.addTags("SAP L1");
+
+        // ---------------------------------------------------------
+        // RELATIONSHIPS
+        // ---------------------------------------------------------
+
+        // System context
+        customer.uses(myWebshop, "Uses");
+        administrator.uses(myWebshop, "Uses");
+        myWebshop.uses(globalPayment, "Uses");
+
+        // Software system (container-level) relationships
+        customer.uses(customerFrontend, "Uses", "https");
+        administrator.uses(administratorFrontend, "Uses", "https");
+        customerFrontend.uses(webshopBackend, "Uses", "http");
+        administratorFrontend.uses(webshopBackend, "Uses", "http");
+        webshopBackend.uses(webshopDatabase, "Uses", "ODBC");
+        webshopBackend.uses(globalPayment, "Uses", "https");
+
+        // ---------------------------------------------------------
+        // VIEWS
+        // ---------------------------------------------------------
+
+        // System Context View
+        SystemContextView contextView = views.createSystemContextView(
+                myWebshop,
+                "MyWebshopSystemContextView",
+                "System Context for My Webshop"
+        );
+        contextView.addAllElements();
+        contextView.enableAutomaticLayout();
+
+        // Container View
+        ContainerView containerView = views.createContainerView(
+                myWebshop,
+                "MyWebshopSoftwareSystemView",
+                "Container View for My Webshop"
+        );
+        containerView.addAllElements();
+        containerView.enableAutomaticLayout();
+
+        // ---------------------------------------------------------
+        // THEMES & STYLES
+        // ---------------------------------------------------------
+
+        views.getConfiguration().setThemes(
+                "https://fabianorosa1.github.io/structurizr-themes/sap-btp-solution-diagrams-2025.12.08/theme.json"
+        );
+
+        // Style override for Database shape (applies to elements tagged "Database")
+        Styles styles = views.getConfiguration().getStyles();
+        styles.addElementStyle("Database").shape(Shape.Cylinder);
+
+        // ---------------------------------------------------------
+        // SAVE WORKSPACE
+        // ---------------------------------------------------------
+
+        com.structurizr.util.WorkspaceUtils.saveWorkspaceToJson(
+                workspace,
+                new File("workspace.json")
+        );
+    }
+}

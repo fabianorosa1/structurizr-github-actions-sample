572a033cccb4bd78172777198ebac6a0ae95d886
Fabiano Rosa <fabianorosa1@gmail.com>
2026-01-29 16:02:45 +0100

    Update container view with additional elements
    Added elements to the container view for S4.

--- src/main/java/com/example/SapCisApplicationsWorkspace.java
+++ src/main/java/com/example/SapCisApplicationsWorkspace.java
@@ -155,7 +155,10 @@ public class SapCisApplicationsWorkspace {
                 "S4SoftwareSystemView",
                 "Container View for S4"
         );
-        containerViewS4.addAllElements();
+        containerViewS4.add(corporateIdp);
+        containerViewS4.add(cisIasApplicationsContainer);
+        containerViewS4.add(cis);
+        containerViewS4.add(CISA_S4HANA_ADC_S59_100);
         containerViewS4.enableAutomaticLayout(RankDirection.TopBottom);
         
         // ---------------------------------------------------------
